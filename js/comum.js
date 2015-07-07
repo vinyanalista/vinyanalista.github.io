505de2842ccf083a2717fc426ef2b16527cabb6b
Antônio Vinícius <vinyanalista@gmail.com>
2015-07-07 11:42:04 -0300

    Atualização do site (ver commit 215956708ad0f0650b73c3ca34941f401b6096fa do repositório vinyanalista-website)

--- js/comum.js
+++ js/comum.js
@@ -9,33 +9,3 @@ var addthis_config = {
     "data_track_addressbar":true,
     "ui_language": "pt"
 };
-
-/* SyntaxHighlighter */
-
-SyntaxHighlighter.config.clipboardSwf = '{{ "/lib/syntaxhighlighter/2.1.382/scripts/clipboard.swf" | prepend: site.baseurl }}';
-SyntaxHighlighter.config.strings.alert = 'Alerta do componente SyntaxHighlighter\n\n';
-SyntaxHighlighter.config.strings.brushNotHtmlScript = 'O brush não suporta a opção html-script: ';
-SyntaxHighlighter.config.strings.copyToClipboard = 'Copiar';
-SyntaxHighlighter.config.strings.copyToClipboardConfirmation = 'O código foi copiado para a sua área de transferência.';
-SyntaxHighlighter.config.strings.expandSource = 'Exibir código-fonte';
-SyntaxHighlighter.config.strings.help = '?';
-SyntaxHighlighter.config.strings.noBrush = 'Não foi possível localizar o brush: ';
-SyntaxHighlighter.config.strings.print = 'Imprimir';
-SyntaxHighlighter.config.strings.viewSource = 'Nova janela';
-SyntaxHighlighter.defaults['auto-links'] = true;
-SyntaxHighlighter.defaults['class-name'] = 'reset-box-sizing';
-SyntaxHighlighter.defaults['pad-line-numbers'] = false;
-SyntaxHighlighter.defaults['wrap-lines'] = false;
-SyntaxHighlighter.all();
-
-/* AdSense */
-
-adsbygoogle = window.adsbygoogle || [];
-
-$(document).ready(function(){
-    $('.adsbygoogle').each(function(index, element){
-        $(element).attr('data-ad-client', adsense_editor_id);
-        $(element).attr('data-ad-slot', adsense_ad_units[index]);
-        adsbygoogle.push({});
-    });
-});
