1748a99c48e5eff69797a61c250bea05f1be3559
Antônio Vinícius <vinyanalista@gmail.com>
2015-10-18 03:02:58 -0200

    Publishing website (see commit b5d6e2d3eaf45450caab38d99522aa71100e2fe2 from vinyanalista-website)

---
+++ assets/js/main.js
@@ -0,0 +1,59 @@
+var $search_input = null;
+var do_search = false;
+
+function hide_search_input() {
+    $('#search-input').animate({
+        'width': 0,
+        'opacity': 0
+    }, 200);
+}
+
+function random_number(first, last) {
+    return Math.floor((Math.random() * (last - first + 1))) + first;
+}
+
+function submit_search_form() {
+    do_search = true;
+    $('#search-form').submit();
+}
+
+$(document).ready(function(){
+    $.material.init();
+
+    /* Search form */
+
+    $search_input = $('#search-input');
+
+    $('#search-button').click(function(event) {
+        event.preventDefault();
+        if ($search_input.width() > 0) {
+            if ($search_input.val()) {
+                submit_search_form();
+            } else {
+                hide_search_input();
+            }
+        } else {
+            $search_input.animate({
+                'width': '182px',
+                'opacity': 1
+            }, 300, function() {
+                $search_input.focus();
+            });
+        }
+    });
+
+    $search_input.keypress(function(event) {
+        if (event.which == 10 || event.which == 13) {
+            submit_search_form();
+        }
+    }).blur(function() {
+        if (!$search_input.val() && !do_search) {
+            hide_search_input();
+        }
+    });
+
+    // Realça links externos
+    $('div.blog-post a').filter(function() {
+        return (this.hostname && (this.hostname != location.hostname));
+    }).append(' <span class="glyphicon glyphicon-new-window"></span>').attr('target', '_blank');
+});
