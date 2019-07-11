3d6d96f96a3f6026bdc1f66ea1556f0680e0562a
Antônio Vinícius Menezes Medeiros <antoniom@tjsc.jus.br>
2019-07-11 16:01:24 -0300

    Publishing website (see commit vinyanalista/vinyanalista-website@8e28b3f92ff8bec0b7a5d9d746b6a38393c76b23)

---
+++ assets/js/main.js
@@ -0,0 +1,12 @@
+function random_number(first, last) {
+    return Math.floor((Math.random() * (last - first + 1))) + first;
+}
+
+$(document).ready(function(){
+    $('body').bootstrapMaterialDesign();
+
+    // Realça links externos
+    $('div.blog-post a').filter(function() {
+        return (this.hostname && (this.hostname != location.hostname) && ($(this).find('img').length == 0));
+    }).append(' <i class="fas fa-external-link-alt"></i>').attr('target', '_blank');
+});
