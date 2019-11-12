be7c7cc70dbcfb7937b1264263b86658628baa00
Antônio Vinícius Menezes Medeiros <vinyanalista@gmail.com>
2019-11-11 22:13:19 -0300

    Publishing website (see commit vinyanalista/vinyanalista-website@f6536807c9c731b372c16e9e46a8dcffd9c582cf)

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,6 +5,12 @@ function random_number(first, last) {
 $(document).ready(function(){
     $('body').bootstrapMaterialDesign();
 
+    $('#fab').click(function(){
+        var enterDesignMode = ($('#fab button i').text() == "visibility");
+        $('#fab button i').text(enterDesignMode ? "edit" : "visibility");
+        document.designMode = enterDesignMode ? "on" : "off";
+    });
+
     // Realça links externos
     $('div.blog-post a').filter(function() {
         return (this.hostname && (this.hostname != location.hostname) && ($(this).find('img').length == 0));
